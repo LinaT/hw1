ff9e99a8ed6ed4a28415d44fb8e7e247097da965
Lina <linathach@yahoo.com>
2013-11-06 22:10:08 -0800

    no message

--- js/viewMenu.js
+++ js/viewMenu.js
@@ -3,30 +3,28 @@ $(function(){
 	renderFood(com.dawgpizza.menu);
 });
 
-function renderFood(com.dawgpizza.menu) {
-
+function renderFood(menu) {
 	var idx;
 	var pizza;
-	var template = $('.template');
+	var template = $('.pizza');
 	var clonedTemplate;
-	var container = $('.com.dawgpizza.menu.pizzas');
+	var container = $('.container-left');
 
-	for (idx = 0; idx < com.dawgpizza.menu.pizzas.length; ++idx) {
-	    pizza = com.dawgpizza.menu.pizzas[idx];
+	for (idx = 0; idx < menu.pizzas.length; ++idx) {
+	    pizza = menu.pizzas[idx];
 	    clonedTemplate = template.clone();
-	    pizza.name = pizza.find('.name').html(pizza.name);
-	    pizza.description = pizza.find('.description').html(pizza.description);
-	    pizza.prices = pizza.find('.prices').html(pizza.prices);
-	    pizza.prices[0] = price for small
-	    pizza.prices[1] = price for medium
-	    pizza.prices[2] = price for large
+	    clonedTemplate.find('.name').html(pizza.name);
+	    clonedTemplate.find('.description').html(pizza.description);
+	    clonedTemplate.find('.prices').html('$'+ pizza.prices[0] + " ");
+	    clonedTemplate.find('.prices').html('$'+ pizza.prices[1] + " ");
+	    clonedTemplate.find('.prices').html('$'+ pizza.prices[2] + " ");
 
 	    clonedTemplate.removeClass('template');
 		container.append(clonedTemplate);
 	} //for each pizza
 
 
-	var idx;
+	/*var idx;
 	var drink;
 	for (idx = 0; idx < com.dawgpizza.menu.drinks.length; ++idx) {
     drink = com.dawgpizza.menu.drinks[idx];
@@ -46,5 +44,5 @@ function renderFood(com.dawgpizza.menu) {
     dessert.price = dessert.find('.price').html(dessert.price);
 
 	} //for each dessert        
-
+*/
 }
